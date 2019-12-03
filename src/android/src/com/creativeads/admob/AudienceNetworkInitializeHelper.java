ee08646e6ad0f2b729b5e25e8e81170bb16e5748
llapps <lauhoffo@gmail.com>
2019-12-03 11:55:32 +0100

    Update and rename AdUnitsSampleApplication.java to AudienceNetworkInitializeHelper.java

--- src/android/src/com/creativeads/admob/AudienceNetworkInitializeHelper.java
+++ src/android/src/com/creativeads/admob/AudienceNetworkInitializeHelper.java
@@ -20,38 +20,36 @@
 
 package com.facebook.samples.AdUnitsSample;
 
-import android.app.Application;
-import android.os.StrictMode;
+import android.content.Context;
+import android.util.Log;
 
+import com.facebook.ads.AdSettings;
 import com.facebook.ads.AudienceNetworkAds;
-import com.facebook.samples.ads.debugsettings.DebugSettings;
 
 import static com.facebook.ads.BuildConfig.DEBUG;
 
-public class AdUnitsSampleApplication extends Application {
+/**
+ * Sample class that shows how to call initialize() method of Audience Network SDK.
+ */
+public class AudienceNetworkInitializeHelper
+    implements AudienceNetworkAds.InitListener {
+
+    /**
+     * It's recommended to call this method from Application.onCreate().
+     * Otherwise you can call it from all Activity.onCreate()
+     * methods for Activities that contain ads.
+     *
+     * @param context Application or Activity.
+     */
+    static void initialize(Context context) {
+        AudienceNetworkAds
+            .buildInitSettings(context)
+            .withInitListener(new AudienceNetworkInitializeHelper())
+            .initialize();
+    }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-
-        if (AudienceNetworkAds.isInAdsProcess(this)) {
-            return;
-        } // else execute default application initialization code
-
-        DebugSettings.initialize(this);
-
-        AudienceNetworkInitializeHelper.initialize(this);
-
-        if (DEBUG) {
-            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-                    .detectAll()
-                    .penaltyLog()
-                    .build());
-            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-                    .detectAll()
-                    .penaltyLog()
-                    .build());
-        }
-
+    public void onInitialized(AudienceNetworkAds.InitResult result) {
+        Log.d(AudienceNetworkAds.TAG, result.getMessage());
     }
 }
