1a9dd0a8ba98f3313e9619af89942201976ceede
llapps <lauhoffo@gmail.com>
2019-12-03 15:11:52 +0100

    Update AudienceNetworkInitializeHelper.java

--- src/android/src/com/creativeads/admob/AudienceNetworkInitializeHelper.java
+++ src/android/src/com/creativeads/admob/AudienceNetworkInitializeHelper.java
@@ -40,9 +40,7 @@ public class AudienceNetworkInitializeHelper implements AudienceNetworkAds.InitL
      * @param context Application or Activity.
      */
     static void initialize(Context context) {
-          AudienceNetworkAds.buildInitSettings(context)
-                .withMediationService("GOOGLE: 17.2.1")
-                .initialize(this);
+          AudienceNetworkAds.initialize();
              
             Log.d("olli", "facebook init");
  
