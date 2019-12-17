64b9ea96e808eaf1b6998db402929757393c803d
llapps <lauhoffo@gmail.com>
2019-12-17 20:12:02 +0100

    Update AdServiceAdMob.java

--- src/android/src/com/creativeads/admob/AdServiceAdMob.java
+++ src/android/src/com/creativeads/admob/AdServiceAdMob.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.util.Log;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.ads.mediationtestsuite;
 import com.creativeads.AdBanner;
 import com.creativeads.AdInterstitial;
 import com.creativeads.AdRewardedVideo;
